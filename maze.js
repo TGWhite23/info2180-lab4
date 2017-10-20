1d85753949f19412198ee3fbdeb0a0f8d3025818
Titania White <titaniawhite@yahoo.com>
2017-10-19 21:38:40 -0500

    Adding my existing site.

--- maze.js
+++ maze.js
@@ -2,6 +2,7 @@ window.onload = function (){
 	alert("hey");
 	var boundary = document.querySelectorAll(".boundary");
 	var i;
+	var touchWall = 0;
 	
 	
 	for (i = 0; i < boundary.length; i++) {
@@ -9,9 +10,22 @@ window.onload = function (){
 	}
 	
 	function turnRed(i) {
+		touchWall ++;
 		for (i = 0; i < boundary.length; i++) {
 		boundary[i].classList.add("youlose");
+		}
 	}
+	
+	var endMaze = document.getElementById("end");		
+	
+	endMaze.onmouseover = youWin;	
+	
+	function youWin () {
+		if (touchWall == 0) {
+		alert ("You Win");		
+		}		
 	}
+	
+	
 }
 
