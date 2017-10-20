c346071c83b0962fda7cbf2a9dea9daa99c93a85
Titania White <titaniawhite@yahoo.com>
2017-10-19 23:51:04 -0500

    Adding my existing site.

--- maze.js
+++ maze.js
@@ -3,39 +3,65 @@ window.onload = function (){
 	var boundary = document.querySelectorAll(".boundary");
 	var i;
 	var touchWall = 0;
+	var startGame = 0;
+	var endGame = 0;
+	var startStatus = document.getElementById("status").innerHTML;
+	var startMaze = document.getElementById("start");
+	var maze = document.getElementById("maze");
 	
+	startMaze.onmouseover = function () {
+		startGame = 1;
+	}
 	
+	maze.onmouseleave = outOfBounds;
+	
+	function outOfBounds(){
+		if (startGame ==1 && endGame == 0){
+			for (i = 0; i < boundary.length; i++) {
+				turnRed(i);
+			}
+		}
+	}
 	for (i = 0; i < boundary.length; i++) {
 		boundary[i].addEventListener("mouseover",turnRed);
 	}
 	
 	function turnRed(i) {
-		touchWall ++;
-		for (i = 0; i < boundary.length; i++) {
-		boundary[i].classList.add("youlose");
+		if (startGame == 1 && endGame == 0){
+			touchWall ++;
+			for (i = 0; i < boundary.length; i++) {
+				boundary[i].classList.add("youlose");
+			}
+			document.getElementById("status").innerHTML = "You Lose!";
 		}
-		document.getElementById("status").innerHTML = "You Lose!";
 	}
 	
 	var endMaze = document.getElementById("end");		
 	
 	endMaze.onmouseover = youWin;	
 	
-	function youWin () {
-		if (touchWall == 0) {
-		document.getElementById("status").innerHTML = "You Win!";;		
+	function youWin () {		
+		if (startGame == 1){			
+			if (touchWall == 0) {
+				endGame = 1;
+				document.getElementById("status").innerHTML = "You Win!";	
+			}	
 		}		
 	}
 	
-	var startMaze = document.getElementById("start");
+	
 	
 	startMaze.onclick = resetMaze;
 	
 	function resetMaze() {
 		for (i = 0; i < boundary.length; i++) {
-		boundary[i].classList.remove("youlose");
+			boundary[i].classList.remove("youlose");
+		}
+		document.getElementById("status").innerHTML = startStatus;
+		startGame = 0;
+		endGame = 0;
 	}
-	}
+	
 	
 	
 	
